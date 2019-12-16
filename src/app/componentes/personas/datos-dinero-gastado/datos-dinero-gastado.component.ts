884a326bd270c4b21a6666c1df551209f5f59622
IvanDuarte1501199 <53486708+IvanDuarte1501199@users.noreply.github.com>
2019-12-15 22:55:15 -0300

    7.1 - anda metodo

--- src/app/componentes/personas/datos-dinero-gastado/datos-dinero-gastado.component.ts
+++ src/app/componentes/personas/datos-dinero-gastado/datos-dinero-gastado.component.ts
@@ -19,7 +19,7 @@ export class DatosDineroGastadoComponent implements OnInit {
   constructor(private _alquileresRepoService: AlquileresRepoService, private _propiedadesRepoService: PropiedadesRepoService) {
     _alquileresRepoService.getAllAlquileres();
     _propiedadesRepoService.getAllPropiedades();
-   }
+  }
 
   ngOnInit() {
   }
@@ -58,26 +58,24 @@ export class DatosDineroGastadoComponent implements OnInit {
   */
   verDatos() {
     this.cantDiasTotal = 0;
-    this.precioCalculado = 1;
+    this.precioCalculado = 0;
     this.cantDias = 0;
     var Ifechaini = new Date(this.fechaInicio);
     var Ifechafin = new Date(this.fechaFin);
 
     if (this.verificarDatos()) {
       this._alquileresRepoService.listadoAlquileres.forEach(alquiler => {
-        console.log('vuelta de forech' + alquiler.id);
         //fechas del alquiler
         var Afechaini = new Date(alquiler.fechaInicio);
         var Afechafin = new Date(alquiler.fechaFin);
         //propiedad de este alquiler
         this._propiedadesRepoService.getPropiedadById(alquiler.propiedadId).subscribe((pro) => {
-          console.log('propiedad' + pro.id);
-          console.log(alquiler.propiedadId);
-          console.log(pro.nombre);
           // comienza toda la comprobacion de dias
           if ((this.fechaInicio >= alquiler.fechaInicio && this.fechaInicio <= alquiler.fechaFin) && (this.fechaFin >= alquiler.fechaInicio && this.fechaFin <= alquiler.fechaFin)) {
             //sacar la diferencia entre dias de la fecha de inicio ingresada con la fecha de fin ingresada
             this.cantDias = Ifechafin.getTime() - Ifechaini.getTime();
+            this.cantDias = Math.round(this.cantDias / (1000 * 60 * 60 * 24));
+            this.cantDias  = this.cantDias + 1;
             this.cantDiasTotal = this.cantDiasTotal + this.cantDias;
             if (this.cantDias > 0) {
               this.precioCalculado = this.precioCalculado + (this.cantDias * pro.precioXdia)
@@ -87,6 +85,8 @@ export class DatosDineroGastadoComponent implements OnInit {
             if ((this.fechaInicio >= alquiler.fechaInicio && this.fechaInicio <= alquiler.fechaFin) && !(this.fechaFin >= alquiler.fechaInicio && this.fechaFin <= alquiler.fechaFin)) {
               // hay que sacar la direncia de dias entre la fecha de inicio y la fecha de fin de el alquiler
               this.cantDias = Ifechafin.getTime() - Afechafin.getTime();
+              this.cantDias = Math.round(this.cantDias / (1000 * 60 * 60 * 24));
+              this.cantDias  = this.cantDias + 1;
               this.cantDiasTotal = this.cantDiasTotal + this.cantDias;
               if (this.cantDias > 0) {
                 this.precioCalculado = this.precioCalculado + (this.cantDias * pro.precioXdia)
@@ -95,21 +95,30 @@ export class DatosDineroGastadoComponent implements OnInit {
               if (!(this.fechaInicio >= alquiler.fechaInicio && this.fechaInicio <= alquiler.fechaFin) && (this.fechaFin >= alquiler.fechaInicio && this.fechaFin <= alquiler.fechaFin)) {
                 //hay que sacar la diferencia entre la fecha de inicio del alquiler y la fecha de fin ingresada
                 this.cantDias = Ifechaini.getTime() - Afechaini.getTime();
+                this.cantDias = Math.round(this.cantDias / (1000 * 60 * 60 * 24));
+                this.cantDias  = this.cantDias + 1;
                 this.cantDiasTotal = this.cantDiasTotal + this.cantDias;
                 if (this.cantDias > 0) {
                   this.precioCalculado = this.precioCalculado + (this.cantDias * pro.precioXdia)
                 }
               } else {
-                if (!(this.fechaInicio >= alquiler.fechaInicio && this.fechaInicio <= alquiler.fechaFin) && !(this.fechaFin >= alquiler.fechaInicio && this.fechaFin <= alquiler.fechaFin)) {
-                  //no sumar nada
-                }
-                else{
+                if (this.fechaInicio <= alquiler.fechaInicio && this.fechaFin >= alquiler.fechaFin) {
+                  this.cantDias = Afechafin.getTime() - Afechaini.getTime();
+                  this.cantDias = Math.round(this.cantDias / (1000 * 60 * 60 * 24));
+                  this.cantDias  = this.cantDias + 1;
+                  this.cantDiasTotal = this.cantDiasTotal + this.cantDias;
+                  if (this.cantDias > 0) {
+                    this.precioCalculado = this.precioCalculado + (this.cantDias * pro.precioXdia)
+                  }
+                  console.log(this.cantDias);
+                  console.log(this.cantDiasTotal);
+                } else {
                 }
               }
             }
           }
         });
-        console.log(this.precioCalculado);
+
       });
       this.ver = true;
 
@@ -128,9 +137,6 @@ export class DatosDineroGastadoComponent implements OnInit {
         alert('La fecha de fin no puede ser menor a la de inicio');
         this.retorno = false;
       }
-    }
-    if (this.retorno == true) {
-      console.log('entro al gran metodo')
     }
     return this.retorno;
   }
